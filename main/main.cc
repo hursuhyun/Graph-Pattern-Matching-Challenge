0dcc859a26238a13828d0ba3f0f42b3097a98d8c
seorin55 <82808344+seorin55@users.noreply.github.com>
2021-05-29 19:27:17 +0900

    ./main/program lcc_hprd_n1

--- main/main.cc
+++ main/main.cc
@@ -9,27 +9,35 @@
 #include "graph.h"
 
 int main(int argc, char* argv[]) {
-  if (argc < 4) {
-    std::cerr << "Usage: ./program <data graph file> <query graph file> "
-                 "<candidate set file>\n";
+  if (argc < 1) {
+    std::cerr << "Usage: ./program <file name base>\n";
     return EXIT_FAILURE;
   }
 
-  std::string data_file_name = argv[1];
-  std::string query_file_name = argv[2];
-  std::string candidate_set_file_name = argv[3];
+  std::string base_file_name = argv[1];
+
+  std::string data_file_name = "", query_file_name = "", candidate_set_file_name = "";
+
+  data_file_name.append(base_file_name);
+  candidate_set_file_name.append(base_file_name);
+  query_file_name.append(base_file_name);
+
+  data_file_name.pop_back();
+  data_file_name.pop_back();
+  data_file_name.pop_back();
+
+  candidate_set_file_name.append(".cs");
+  query_file_name.append(".igraph");
+  data_file_name.append(".igraph");
 
   Graph data(data_file_name);
   Graph query(query_file_name, true);
   CandidateSet candidate_set(candidate_set_file_name);
 
   Backtrack backtrack;
-  
-   time_t start = time(NULL);
-  backtrack.PrintAllMatches(query_file_name, data, query, candidate_set);
-   time_t end = time(NULL);
 
-  double time = (double)(end - start);
-  std::cout << time << std::endl;
+  backtrack.PrintAllMatches(query_file_name, data, query, candidate_set);
+
   return EXIT_SUCCESS;
 }
+
