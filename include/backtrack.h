bc0ee0f9ecabe0e08676dc9ac63daaf7a23ffdbe
seorin55 <82808344+seorin55@users.noreply.github.com>
2021-06-02 23:34:20 +0900

    Add files via upload

--- include/backtrack.h
+++ include/backtrack.h
@@ -9,14 +9,20 @@
 #include "candidate_set.h"
 #include "common.h"
 #include "graph.h"
+#include <queue>
+#include <vector>
 
 class Backtrack {
  public:
   Backtrack();
   ~Backtrack();
 
-  void PrintAllMatches(const Graph &data, const Graph &query,
+  void PrintAllMatches(std::string filename, const Graph &data, const Graph &query,
                        const CandidateSet &cs);
+  void bfsTraversal(const Graph &query, Vertex root_vertex, Tree *&tree, Vertex *&bfs_order);
+  std::vector<Vertex> FirstCSMin(const Graph &data, const Graph &query, const CandidateSet &cs);
+  std::vector<Vertex> SecondRIMin(const Graph &data, const Graph &query, const CandidateSet &cs);
+
 };
 
 #endif  // BACKTRACK_H_
