209a413ef1fdff6c3a76e313add06c3b36c87493
shhur <hursuhyun@gmail.com>
2021-06-04 14:47:52 +0900

    final

--- main/main.cc
+++ main/main.cc
@@ -8,29 +8,16 @@
 #include "common.h"
 #include "graph.h"
 
-
-
 int main(int argc, char* argv[]) {
-  if (argc < 1) {
-    std::cerr << "Usage: ./program <file name base>\n";
+  if (argc < 4) {
+    std::cerr << "Usage: ./program <data graph file> <query graph file> "
+                 "<candidate set file>\n";
     return EXIT_FAILURE;
   }
 
-  std::string base_file_name = argv[1];
-
-  std::string data_file_name = "", query_file_name = "", candidate_set_file_name = "";
-
-  data_file_name.append(base_file_name);
-  candidate_set_file_name.append(base_file_name);
-  query_file_name.append(base_file_name);
-
-  data_file_name.pop_back();
-  data_file_name.pop_back();
-  data_file_name.pop_back();
-
-  candidate_set_file_name.append(".cs");
-  query_file_name.append(".igraph");
-  data_file_name.append(".igraph");
+  std::string data_file_name = argv[1];
+  std::string query_file_name = argv[2];
+  std::string candidate_set_file_name = argv[3];
 
   Graph data(data_file_name);
   Graph query(query_file_name, true);
@@ -38,7 +25,6 @@ int main(int argc, char* argv[]) {
 
   Backtrack backtrack;
 
-  backtrack.PrintAllMatches(query_file_name, data, query, candidate_set);
-
+  backtrack.PrintAllMatches(data, query, candidate_set);
   return EXIT_SUCCESS;
 }
